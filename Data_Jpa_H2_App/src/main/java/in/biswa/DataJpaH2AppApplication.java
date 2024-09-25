ede778ebd029bbc7beae2fc26bacf337a9ad97ac
=Biswaranjan <biswaranjanb627@gmail.com>
2024-09-25 07:46:38 +0530

    25/09/2024

---
+++ Data_Jpa_H2_App/src/main/java/in/biswa/DataJpaH2AppApplication.java
@@ -0,0 +1,13 @@
+package in.biswa;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DataJpaH2AppApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DataJpaH2AppApplication.class, args);
+	}
+
+}
