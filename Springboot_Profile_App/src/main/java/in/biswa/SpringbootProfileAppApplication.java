f07435a4fd0a8d61519ce28bbede784658067f17
=Biswaranjan <biswaranjanb627@gmail.com>
2024-09-26 12:55:28 +0530

    26/09/2024

---
+++ Springboot_Profile_App/src/main/java/in/biswa/SpringbootProfileAppApplication.java
@@ -0,0 +1,13 @@
+package in.biswa;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringbootProfileAppApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringbootProfileAppApplication.class, args);
+	}
+
+}
