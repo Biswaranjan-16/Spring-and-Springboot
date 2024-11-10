a1b2ffe30dd4ae14a7677ef9ecbea26f042a6430
=Biswaranjan <biswaranjanb627@gmail.com>
2024-11-11 02:12:03 +0530

    11/11/2024

---
+++ MVC/src/main/java/in/biswa/controller/Welcomeconntroller.java
@@ -0,0 +1,22 @@
+package in.biswa.controller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.servlet.ModelAndView;
+
+@Controller
+public class Welcomeconntroller {
+
+	public Welcomeconntroller() {
+		System.out.println("welcome controller : constructor");
+	}
+	@GetMapping("/welcome")
+	public ModelAndView getWelComeMsg() {
+		ModelAndView mav = new ModelAndView();
+		
+		mav.addObject("msg","Welcome to my world");
+		mav.setViewName("index");
+		return mav;
+	}
+
+}
